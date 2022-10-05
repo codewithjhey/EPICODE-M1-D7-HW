4cd2187ca470cbd442b03c811dcf1206a0c7daac
codewithjhey <114162135+codewithjhey@users.noreply.github.com>
2022-10-05 14:12:39 +0100

    6 Exercises done

---
+++ index.js
@@ -0,0 +1,47 @@
+/* Datatypes, Operators and Conditionals Exercises */
+
+/* EXERCISE 1
+ Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
+*/
+
+/* 1. String - This is a sequence of one or more character that may consist of letter,number or symbols. they can be inside a single, double quotes or backticks.
+2. Boolean - is a data type which has one of only two possible values: true or false also called flags.
+3. Undefined - this is simply an undeclared variable. that means a variable that has not been assigned a value 'value not defined yet'.
+4. Null - "an empty value" used when you wish to point to the absence of a value.
+5. Number - this data type accomodates integers, decimals (floating)
+*/
+
+/* EXERCISE 2
+ Try to describe what a variable is, in your own words.
+*/
+
+/* Variable can be called named storage for data or container that carries or store data value that can be changed laer on ( a label)*/
+
+/* EXERCISE 3
+ Write the code to execute an addition (a sum) of the numbers 12 and 20.
+*/
+
+let sum = 12 + 20
+console.log(sum)
+
+/* EXERCISE 4
+ Create a variable named x and assign to it the number 12.
+*/
+
+let victor = x
+let x = 12
+console.log("assignment of x a value of 12", x)
+
+/* EXERCISE 5
+ Create a variable called name and assign to it the string "John Doe".
+*/
+
+let name = "John Doe"
+console.log("this variable has been assigned a string", "John Doe")
+
+/* EXERCISE 6
+ Execute a subtraction between the number 12 and the variable x you declared before (which is storing the value 12).
+*/
+
+let difference = 12 - 12
+console.log(difference)
